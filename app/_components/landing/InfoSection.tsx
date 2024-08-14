260bc2310c516b059e5367d50606f1b590511719
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-08-14 20:47:42 +0400

    info sections

---
+++ app/_components/landing/InfoSection.tsx
@@ -0,0 +1,15 @@
+import Container from "@/app/_components/Container";
+
+const InfoSection = () => {
+  return (
+    <section>
+      <Container>
+        <div className="grid grid-cols-2 gap-8">
+          <div className="flex flex-col"></div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default InfoSection;
