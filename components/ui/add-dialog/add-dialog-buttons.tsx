2540b338a5393f362cb1ba7862c0dbae4bb35204
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-08-31 18:48:24 +0400

    dialog dropdown

--- components/ui/add-dialog/add-dialog-buttons.tsx
+++ components/ui/add-dialog/add-dialog-buttons.tsx
@@ -1,29 +1,70 @@
 import Button from "@/components/button";
 import { PiCalendarBlankThin } from "react-icons/pi";
-import { CiFlag1 } from "react-icons/ci";
-import { IoAlarmOutline } from "react-icons/io5";
+import { CiAt, CiFlag1 } from "react-icons/ci";
+import { IoAlarmOutline, IoPricetag } from "react-icons/io5";
 import Badge from "@/components/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import IconButton from "@/components/icon-button";
+import { Location } from "@/components/icons/geist-icons";
 
 export default function AddDialogButtons() {
   return (
-    <div className="flex gap-2">
-      <Button
-        size="sm"
-        icon={<PiCalendarBlankThin />}
-        primary
-        onClick={() => {}}
-      >
-        Due date
-      </Button>
-      <Button size="sm" icon={<CiFlag1 />} primary>
-        Priority
-      </Button>
-      <Button size="sm" icon={<IoAlarmOutline />} primary>
-        Reminders{" "}
-        <Badge color="amber-900" bg="amber-200">
-          UPGRADE
-        </Badge>
-      </Button>
-    </div>
+    <DropdownMenu>
+      <div className="flex gap-2">
+        <Button
+          size="sm"
+          icon={<PiCalendarBlankThin />}
+          primary
+          onClick={() => {}}
+        >
+          Due date
+        </Button>
+        <Button size="sm" icon={<CiFlag1 />} primary>
+          Priority
+        </Button>
+        <Button size="sm" icon={<IoAlarmOutline />} primary>
+          Reminders{" "}
+          <Badge color="amber-900" bg="amber-200">
+            UPGRADE
+          </Badge>
+        </Button>
+        <DropdownMenuTrigger>
+          <IconButton>
+            <HiOutlineDotsHorizontal />
+          </IconButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="min-w-52">
+          <DropdownMenuGroup className="flex flex-col">
+            <DropdownMenuItem asChild>
+              <button className="grid grid-cols-[1.5rem_1fr] justify-items-start">
+                <IoPricetag />
+                <span className="inline-flex items-center w-full justify-between">
+                  Labels
+                  <CiAt />
+                </span>
+              </button>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <button className="grid grid-cols-[1.5rem_1fr] justify-items-start">
+                <Location />
+                <span className="inline-flex items-center w-full justify-between">
+                  Location
+                  <Badge color="amber-900" bg="amber-200">
+                    UPGRADE
+                  </Badge>
+                </span>
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </div>
+    </DropdownMenu>
   );
 }
