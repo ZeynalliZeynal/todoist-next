463727aecf889ea65bc9bf9cc887dc3f304eb440
Zeynalli Zeynal <zzeynalli446@gmail.com>
2024-09-19 03:41:26 +0400

    missing suspense boundary

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import AuthPage from "@/components/auth/auth-page";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Log in",
 };
 
 const Page = () => {
-  return <AuthPage type='login' />;
+  return (
+    <Suspense>
+      <AuthPage type='login' />
+    </Suspense>
+  );
 };
 
 export default Page;
